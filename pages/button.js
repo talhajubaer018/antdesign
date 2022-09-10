16cc9e07ded3274ed24bad331f4566d14ebe76a3
talhajubaer018 <talhajubaer018@gmail.com>
2022-09-10 12:37:52 +0600

    button added

---
+++ pages/button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Button } from 'antd'
+
+const button = () => {
+  return (
+    <div>
+         <Button type="primary">Primary Button</Button>
+         <Button type="dashed">Dashed Button</Button>
+         <Button type="link">Link Button</Button>
+    </div>
+  )
+}
+
+export default button
