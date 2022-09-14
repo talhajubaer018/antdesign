373be31dc14208059469f087822b83ca8b9f3e60
talhajubaer018 <talhajubaer018@gmail.com>
2022-09-14 18:37:28 +0600

    treeselect added

---
+++ pages/treeselect.js
@@ -0,0 +1,52 @@
+import { TreeSelect } from 'antd';
+import React, { useState } from 'react';
+const treeData = [
+  {
+    title: 'Node1',
+    value: '0-0',
+    children: [
+      {
+        title: 'Child Node1',
+        value: '0-0-1',
+      },
+      {
+        title: 'Child Node2',
+        value: '0-0-2',
+      },
+    ],
+  },
+  {
+    title: 'Node2',
+    value: '0-1',
+  },
+];
+
+const TreeSelectAD = () => {
+  const [value, setValue] = useState();
+
+  const onChange = (newValue) => {
+    console.log(newValue);
+    setValue(newValue);
+  };
+
+  return (
+    <TreeSelect
+    treeLine
+    showLeafIcon
+      style={{
+        width: '100%',
+      }}
+      value={value}
+      dropdownStyle={{
+        maxHeight: 400,
+        overflow: 'auto',
+      }}
+      treeData={treeData}
+      placeholder="Please select"
+      treeDefaultExpandAll
+      onChange={onChange}
+    />
+  );
+};
+
+export default TreeSelectAD;
