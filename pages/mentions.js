5aa02dc2e1ef69893e71e3e4fd3ae96a4abaf120
talhajubaer018 <talhajubaer018@gmail.com>
2022-09-14 12:39:01 +0600

    mentions added

---
+++ pages/mentions.js
@@ -0,0 +1,28 @@
+import { Mentions } from 'antd';
+import React from 'react';
+const { Option } = Mentions;
+
+const onChange = (value) => {
+  console.log('Change:', value);
+};
+
+const onSelect = (option) => {
+  console.log('select', option);
+};
+
+const MentionsAD = () => (
+  <Mentions
+    style={{
+      width: '100%',
+    }}
+    onChange={onChange}
+    onSelect={onSelect}
+    defaultValue="@afc163"
+  >
+    <Option value="afc163">afc163</Option>
+    <Option value="zombieJ">zombieJ</Option>
+    <Option value="yesmeck">yesmeck</Option>
+  </Mentions>
+);
+
+export default MentionsAD
