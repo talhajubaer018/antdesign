a0618415c47bc88cc638bd24cf91e4a854bbd06b
talhajubaer018 <talhajubaer018@gmail.com>
2022-09-14 22:46:35 +0600

    popover added

---
+++ pages/popover.js
@@ -0,0 +1,24 @@
+import { Button, Popover } from 'antd';
+import React from 'react';
+const content = (
+  <div>
+    <p>Content</p>
+    <p>Content</p>
+  </div>
+);
+
+const PopoverAD = () => (
+  <div>
+    <Popover content={content} title="Title" trigger="hover">
+      <Button>Hover me</Button>
+    </Popover>
+    <Popover content={content} title="Title" trigger="focus">
+      <Button>Focus me</Button>
+    </Popover>
+    <Popover content={content} title="Title" trigger="click">
+      <Button>Click me</Button>
+    </Popover>
+  </div>
+);
+
+export default PopoverAD;
